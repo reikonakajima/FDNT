d8f52f3a5a14ffe4df55881215aecc13cb50fdda
Reiko Nakajima <reiko.nakajima@gmail.com>
2013-09-05 16:27:53 -0400

    add pysrc/module.cpp (#6)

---
+++ pysrc/module.cpp
@@ -0,0 +1,26 @@
+#include "boost/python.hpp"
+//#include "numpy/arrayobject.h"
+
+
+// declare the export functions
+
+namespace galsim {
+
+  namespace fdnt {
+    
+    void pyExportRunFDNT();
+    void pyExportFDNTImage();
+
+  } 
+
+}
+
+
+// generate the galsim FDNT module
+
+BOOST_PYTHON_MODULE(_fdnt) {
+
+  galsim::fdnt::pyExportRunFDNT();
+  galsim::fdnt::pyExportFDNTImage();
+
+}
