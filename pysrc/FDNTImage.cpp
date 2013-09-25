eac139f0607225e54c4c69e1430c3da417a4b359
Reiko Nakajima <reiko.nakajima@gmail.com>
2013-09-25 15:06:49 +0200

    started wrapping FDNTImage, but python initialization fails (#6)

--- pysrc/FDNTImage.cpp
+++ pysrc/FDNTImage.cpp
@@ -8,17 +8,64 @@ namespace bp = boost::python;
 
 
 namespace galsim {
-  namespace fdnt {
+namespace fdnt {
+
+
+template <typename T>
+struct PyFDNTImage {
+
+  template <typename U>
+  static Image<T>* MakeFromImage(const Image<U>& rhs)
+  { return new Image<T>(rhs); }
+
+  template <typename U, typename W>
+  static void wrapImageTemplates(W& wrapper) {
+    typedef Image<T>* (*constructFrom_func_type)(const Image<U>&);
+    //typedef void (<Image<T>::* copyFrom_func_type)(const Image<U>&);
+    wrapper
+      .def(
+	   "__init__",
+	   bp::make_constructor(
+		constructFrom_func_type(&MakeFromImage),
+		bp::default_call_policies(), bp::args("other")
+				)
+	   )
+      //.def("copyFrom", copyFrom_func_type(&Image<T>::copyFrom));
+      ;
+  } // wrapImageTemplates()
+
+  static bp::object wrapFDNTImage(const std::string& suffix) {
+
+    bp::class_< Image<T> >   // in Gary's code, it's an "Image<>" class
+      PyFDNTImage(("FDNTImage" + suffix).c_str(), "", bp::no_init);
+    PyFDNTImage
+      .def(bp::init<int, int>(bp::args("ncol", "nrow")))  // XXX check out the return type XXX
+      ;
+
+    wrapImageTemplates<float>(PyFDNTImage);
+
+  } // wrapFDNTImage()
+
+}; // struct PyFDNTImage
+
 
 
 void pyExportFDNTImage() {
 
-  // define Gary's old Image class with BoostPython
-  // bp::class_<Image>("FDNTImage") ...;
   /*
-  bp::class_<Image<float> >("FDNTImageF");
-  bp::class_<Image<int> >("FDNTImageI");
+  PyFDNTImageHeader::wrapFDNTImageHeader;  // returns "FDNTImageHeader" class
+
+  PyFDNTImageData<int16_t>::wrapFDNTImageData("S");  // returns "FDNTImageDataS" class
+  PyFDNTImageData<int32_t>::wrapFDNTImageData("I");  // returns "FDNTImageDataI" class ... etc
+  PyFDNTImageData<float>::wrapFDNTImageData("F");
+  PyFDNTImageData<double>::wrapFDNTImageData("D");
+
+  PyFDNTImage<int16_t>::wrapFDNTImage("S");  // returns "FDNTImageS" class
+  PyFDNTImage<int32_t>::wrapFDNTImage("I");  // returns "FDNTImageI" class ... etc
   */
+  PyFDNTImage<float>::wrapFDNTImage("F");
+  //PyFDNTImage<double>::wrapFDNTImage("D");
+
 }
 
 
