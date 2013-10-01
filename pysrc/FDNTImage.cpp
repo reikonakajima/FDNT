d994dc79d7d4085b9ed19ae5133a880e651857c1
Reiko Nakajima <reiko.nakajima@gmail.com>
2013-10-01 12:54:25 +0200

    attempt to add_property("array"), compile fails (#6)

--- pysrc/FDNTImage.cpp
+++ pysrc/FDNTImage.cpp
@@ -38,6 +38,29 @@ struct PyFDNTImageData {
 template <typename T>
 struct PyFDNTImage {
 
+  static bp::object GetArrayImpl(bp::object self, bool isConst)
+  {
+    // --- Try to get cached array ---
+    if (PyObject_HasAttrString(self.ptr(), "_array") && self.attr("_array") != bp::object())
+      return self.attr("_array");
+
+    const Image<T>& image = bp::extract<const Image<T>&>(self);
+
+    const T* data = image.data()->location(image.getBounds().getXMin(), image.getBounds().getYMin());
+    int n1 = image.getBounds().getYMax() - image.getBounds().getYMin() + 1;
+    int n2 = image.getBounds().getXMax() - image.getBounds().getXMin() + 1;
+    int stride = n2;  // this will NOT work for subimages!  TEMOPRARY SOLUTION, checked by next line
+    if (!image.data()->contiguousData()) {
+      throw ImageError("stride does not match X dimension");
+    }
+    bp::object numpy_array = MakeNumpyArray(data, n1, n2, stride, isConst);  // will always be owner.
+
+    self.attr("_array") = numpy_array;
+    return numpy_array;
+  }
+
+  static bp::object GetArray(bp::object image) { return GetArrayImpl(image, false); }
+
   static Image<T>* MakeFromImage(const Image<T>& rhs)
   { return new Image<T>(rhs); }
 
@@ -63,6 +86,7 @@ struct PyFDNTImage {
       pyFDNTImage(("FDNTImage" + suffix).c_str(), "", bp::no_init);
     pyFDNTImage
       .def(bp::init<int, int>(bp::args("ncol", "nrow")))
+      .add_property("array", &GetArray)
       ;
 
     // These lines allows for copy constructors between different types.  None allowed for now.
