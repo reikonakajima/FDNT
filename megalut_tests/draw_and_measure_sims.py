d7cae6729bb0ac71e0563a0ec6f58ac06899e1dc
Reiko Nakajima <reiko.nakajima@gmail.com>
2014-09-26 11:09:29 +0200

    re-write megalut test wrapper for debugging (#22)

--- megalut_tests/draw_and_measure_sims.py
+++ megalut_tests/draw_and_measure_sims.py
@@ -7,33 +7,49 @@ logging.basicConfig(level=logging.INFO)
 
 import megalut
 import megalut.sim
-import megalut.meas
 import sys
 import fdnt_glmom
+import numpy as np
+import fdnt
+import galsim
+
+#
+# debug flags
+#
+create_new_catalog_and_image = True
+
 
 # First, we set the desired distributions of parameters, by overwriting the default distributions.
 
 class MySimParams(megalut.sim.params.Params):
 	def get_flux(self):
 		return 300.0
+	def get_g(self):
+		return np.random.uniform(low=-0.05, high=0.05, size=2)
 		
 mysimparams = MySimParams()
 
-simcat = megalut.sim.stampgrid.drawcat(mysimparams, n=10, stampsize=48)
+if create_new_catalog_and_image:
+	simcat = megalut.sim.stampgrid.drawcat(mysimparams, n=10, stampsize=48)
+	megalut.utils.writepickle(simcat, "simcat.pkl")
+else:
+	simcat = megalut.utils.readpickle("simcat.pkl")
 
 print simcat[:5]
 
-# Now, we pass this catalog to drawimg, to generate the actual smulated images.
+# Now, we pass this catalog to drawimg, to generate the actual simulated images.
 
-megalut.sim.stampgrid.drawimg(simcat,
-	simgalimgfilepath="simgalimg.fits",
-	simtrugalimgfilepath="simtrugalimg.fits",
-	simpsfimgfilepath="simpsfimg.fits"
-	)
+if create_new_catalog_and_image:
+	megalut.sim.stampgrid.drawimg(simcat,
+				      simgalimgfilepath="simgalimg.fits",
+				      simtrugalimgfilepath="simtrugalimg.fits",
+				      simpsfimgfilepath="simpsfimg.fits"
+				      )
 
 # We can directly proceed by measuring the images
 
-gridimg = megalut.meas.galsim_adamom.loadimg("simgalimg.fits")
+gridimg = fdnt_glmom.loadimg("simpsfimg.fits")
+
 meascat = fdnt_glmom.measure(gridimg, simcat, stampsize=48, prefix="mes")
 
 # meascat is the output catalog, it contains the measured features:
@@ -51,5 +67,4 @@ plt.scatter(resi_x, resi_y, c=flag, lw=0, s=30)
 plt.xlabel("mes_x residual")
 plt.ylabel("mes_y residual")
 plt.show()
-
 
