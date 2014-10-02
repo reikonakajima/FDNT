ea44f44d5c6a3f53e6cb2945f58eb17a6392d33e
Reiko Nakajima <reiko.nakajima@gmail.com>
2014-10-02 16:28:03 +0200

    simple test to compare against GalSim:AdaptiveMom() (#22)

--- megalut_tests/draw_and_measure_sims.py
+++ megalut_tests/draw_and_measure_sims.py
@@ -19,26 +19,52 @@ import galsim
 #
 create_new_catalog_and_image = True
 
+#
+# run conditions
+#
+n = 10  # 1d size of the sample postage stamp array (there will be n x n galaxies)
+
 
 # First, we set the desired distributions of parameters, by overwriting the default distributions.
 
 class MySimParams(megalut.sim.params.Params):
+	def get_sig(self):
+		return 1.0
+
+	def get_rad(self):
+		return np.random.uniform(0.5, 5.0)
+
 	def get_flux(self):
-		return 6000.0
+		return np.random.uniform(10.0, 200.0)
+
 	def get_g(self):
 		return np.random.uniform(low=-0.4, high=0.4, size=2)
-	def get_rad(self):
-		return np.random.uniform(2.5, 5.0)
+
+	def get_sersicn(self, ix=0, iy=0, n=1):
+		"""
+		This is a bit special: we do not draw sersic indices randomly, as changing it
+		from stamp to stamp significantly slows down galsim ! That's why we need to know
+		the stamp index.
+
+		:param ix: x index of the galaxy, going from 0 to n-1
+		:param iy: y index, idem
+		:param n: n x n is the number of stamps
+
+		"""
+		pseudorand = float(iy)/float(n)
+		return 0.5 + pseudorand * 5.0
+
+
 		
 mysimparams = MySimParams()
 
 if create_new_catalog_and_image:
-	simcat = stampgrid.drawcat(mysimparams, n=10, stampsize=48)
+	simcat = stampgrid.drawcat(mysimparams, n=n, stampsize=48)
 	megalut.utils.writepickle(simcat, "simcat.pkl")
 else:
 	simcat = megalut.utils.readpickle("simcat.pkl")
 
-print simcat[:5]
+print simcat
 
 # Now, we pass this catalog to drawimg, to generate the actual simulated images.
 
@@ -56,13 +82,16 @@ gridimg = fdnt_glmom.loadimg("simgalimg.fits")
 meascat = fdnt_glmom.measure(gridimg, simcat, stampsize=48, prefix="mes")
 
 # meascat is the output catalog, it contains the measured features:
-print meascat[:5]
+print meascat
 
 # We save it into a pickle
 megalut.utils.writepickle(meascat, "meascat.pkl")
 
 # Let's make a simple comparision plot:
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 resi_x = meascat["mes_x"] - meascat["x"]
 resi_y = meascat["mes_y"] - meascat["y"]
 flag = meascat["mes_flag"]
